8629a23d0377cecde7f571bb3cb92eb1853606d8
CraneCD <craneytcd@gmail.com>
2025-05-27 14:35:33 -0500

    fix: Update CSP headers to allow YouTube video embeds

--- next.config.js
+++ next.config.js
@@ -92,10 +92,7 @@ const nextConfig = {
             key: 'X-XSS-Protection',
             value: '1; mode=block'
           },
-          {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
-          },
+
           {
             key: 'X-Content-Type-Options',
             value: 'nosniff'
@@ -106,7 +103,7 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self'"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self'; frame-src 'self' https://www.youtube.com https://youtube.com; media-src 'self' https://www.youtube.com https://youtube.com"
           }
         ]
       }
